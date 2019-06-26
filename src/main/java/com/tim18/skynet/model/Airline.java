67d64f59c3fb93cf578da4c348a5eaf5825d0245
Dusan Stevic <stevicdule@gmail.com>
2019-06-26 16:41:17 +0200

    16:40

--- src/main/java/com/tim18/skynet/model/Airline.java
+++ src/main/java/com/tim18/skynet/model/Airline.java
@@ -9,6 +9,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -18,8 +19,11 @@ public class Airline implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", unique=true, nullable=false)
 	private Long id;
 	@Column(nullable = false)
 	private String name;
--- src/main/java/com/tim18/skynet/model/AirlineAdmin.java
+++ src/main/java/com/tim18/skynet/model/AirlineAdmin.java
@@ -9,8 +9,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class AirlineAdmin extends User{
-private static final long serialVersionUID = -1831516149566167290L;
+
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@JsonIgnore
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "airline_id", referencedColumnName = "id")
